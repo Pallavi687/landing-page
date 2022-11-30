ca67df352440701fe03b5386ff3f3f4490127488
Pallavi687 <117720494+Pallavi687@users.noreply.github.com>
2022-11-30 14:50:55 +0530

    added all files

---
+++ LandingPage/index.js
@@ -0,0 +1,15 @@
+
+
+function submit(){
+
+   let first_name = document.getElementById("name").value;
+   let last_name = document.getElementById("lastname").value;
+   let email = document.getElementById("email").value;
+   let Reason = document.getElementById("reason").value;
+
+     console.log("firstname:", first_name,
+               "last_name:",last_name,
+               "email:",email,
+               "reason:",Reason
+               )
+}
